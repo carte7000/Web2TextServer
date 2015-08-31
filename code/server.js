c40593cf8d39619f0653ae5f8426e372585f4498
carte7000 <simrob7000@gmail.com>
2015-08-31 00:16:13 -0400

    First Commit

---
+++ code/server.js
@@ -0,0 +1,79 @@
+var express  = require('express');
+var Firebase = require('firebase');
+var FirebaseTokenGenerator = require("firebase-token-generator");
+var GCM = require('gcm').GCM;
+
+var apiKey = 'AIzaSyDMG4x7G71v8NCVlObsjBXu0O8-SLSTKk8';
+var gcm = new GCM(apiKey);
+var app = express();
+
+var port = 1337;
+
+app.listen(port, '0.0.0.0', function(){
+	console.log("Server started on port " + port);
+});
+
+app.use(express.static('./build/statics'));
+
+app.get('/', function(req, res) {
+  res.sendfile('./build/index.html');
+});
+
+var tokenGenerator = new FirebaseTokenGenerator("36rGPIywdkXJkY4ozzxkiF9LuPyvxZ9u5mPn26da");
+var AUTH_TOKEN = tokenGenerator.createToken({uid: "admin1"},{admin:true});
+
+var ref = new Firebase('https://web2text.firebaseio.com/');
+ref.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
+	if (error) {
+		console.log("Login Failed!", error);
+	} else {
+		console.log("Login Succeeded!", authData);
+		
+	}
+	registerUserAdded(ref);
+});
+
+var registerUserAdded = function(ref){
+	ref.on("child_added", function(userList){
+		console.log("%j", userList.key());
+		registerNewConversationForUser(ref, userList.key());
+	});
+}
+
+var registerNewConversationForUser = function(ref, userId){
+	var userRef = ref.child(userId);
+	userRef.child("conversations").on("child_added", function(conversation){
+		var conversationRef = userRef.child("conversations").child(conversation.key());
+		registerNewMessageForUser(conversationRef, userRef);
+	})
+}
+
+var registerNewMessageForUser = function(ref, userRef){
+	ref.child("messages").orderByChild("sent_date").startAt((new Date).getTime()).on("child_added", function(message){
+		console.log("%j", message.val());
+		var mes = message.val();
+		userRef.child("gcmId").once('value', function(valueSnap){
+			mes.registration_id = valueSnap.val();
+			var data = {
+				registration_id: valueSnap.val(),
+				'data.receiverNumber': normalize(mes.receiverNumber),
+				'data.content': mes.content,
+				'data.source': mes.source
+			};
+			if(mes.source == "web"){
+				gcm.send(data, function(err, messageId){
+    				if (err) {
+        				console.log("Something has gone wrong!");
+    				} else {
+        				console.log("Sent with message ID: ", messageId);
+    				}
+    			});
+			}
+		});
+	});
+
+	}
+
+var normalize = function(value){
+	return value.substr(value.length - 10);
+}	
---
+++ src/js/main.js
@@ -0,0 +1,120 @@
+var angular = require("angular");
+require("ngRoute");
+require("firebase");
+require("angularfire");
+require("angular-ui-router");
+
+var web2textApp = angular.module('web2text', ["ngRoute", "firebase", 'ui.router']);
+var loginId = null;
+
+web2textApp.controller('loginController', function($scope) {
+	$scope.login = {
+      email:"",
+      password:""
+    }
+    
+    $scope.login = function(){
+      var rootRef = new Firebase('https://web2text.firebaseio.com/');
+      rootRef.authWithPassword($scope.login, function onAuth(error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+            } else {
+              console.log("Authenticated successfully with payload:", authData);
+              loginId = authData.uid;
+              window.location = "/#/chatroom";
+            }
+      });
+    }
+});
+
+web2textApp.controller('chatroomController', function($scope, $firebaseObject) {
+	var rootRef = new Firebase('https://web2text.firebaseio.com/');
+
+  $scope.selected = 0;
+
+    $scope.select= function(index) {
+       $scope.selected = index; 
+    };
+
+  var user = rootRef.getAuth();
+  if(user==null){
+    window.location = "/";
+  }
+  else{
+	 var userConversationsRef = rootRef.child(user.uid).child("conversations");
+
+	 var syncObject = $firebaseObject(userConversationsRef);
+	 syncObject.$bindTo($scope, "conversations");
+  }
+});
+
+web2textApp.controller('chatBoxController', function($scope, $firebaseObject, $state, $stateParams) {
+  var rootRef = new Firebase('https://web2text.firebaseio.com/');
+
+  var user = rootRef.getAuth();
+  if(user==null){
+    window.location = "/";
+  }
+  else{
+   var conversationId = $stateParams.contactid;
+   $scope.conversationId = conversationId;
+   var userConversationsRef = rootRef.child(user.uid).child("conversations").child(conversationId).child("messages");
+
+   var syncObject = $firebaseObject(userConversationsRef);
+   syncObject.$bindTo($scope, "messages");
+   userConversationsRef.on("child_added", function(data){
+      window.setTimeout(function() {
+        var elem = document.getElementById('chat');
+        elem.scrollTop = elem.scrollHeight;
+      }, 200);
+   });
+
+   window.setTimeout(function() {
+      var elem = document.getElementById('chat');
+      elem.scrollTop = elem.scrollHeight;
+   }, 200);
+
+   $scope.text = "";
+
+   $scope.decrypt = function(value){
+      return value;//CryptoJS.AES.decrypt(value, rootRef.getAuth().uid).toString(CryptoJS.enc.Utf8);
+   }
+
+   $scope.send = function(){
+      var encryptedMessage = CryptoJS.AES.encrypt($scope.text, rootRef.getAuth().uid);
+      userConversationsRef.push().set({
+        content: $scope.text,//encryptedMessage.toString(),
+        receiverNumber:conversationId,
+        sent_date: Firebase.ServerValue.TIMESTAMP,
+        source: "web"
+      });
+      $scope.text = "";  
+    }
+  }
+});
+
+web2textApp.config(['$routeProvider', '$stateProvider',
+  		function($routeProvider, $stateProvider) {
+    		$routeProvider.
+      		when('/home', {
+        		templateUrl: '/partials/home.html',
+        		controller: 'loginController'
+      		}).
+      		when('/chatroom', {
+      			templateUrl: '/partials/chatroom.html',
+        		controller: 'chatroomController'
+      		}).
+      		otherwise({
+        		redirectTo: '/home'
+      		});
+
+          $stateProvider
+            .state('contacts', {
+              templateUrl: '/partials/chatBox.html',
+              params: {
+                contactid: null
+              },
+              controller: 'chatBoxController'
+
+            });
+  	}]);
